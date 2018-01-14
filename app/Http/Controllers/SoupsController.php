ed1110f930f24d18b848fe6593389d11a9d360ac
Luis <rivasl.works@gmail.com>
2018-01-14 20:58:12 -0300

    Added Soup Controller

--- app/Http/Controllers/SoupsController.php
+++ app/Http/Controllers/SoupsController.php
@@ -13,38 +13,10 @@ class SoupsController extends Controller
     *
     * @return \Illuminate\Http\Response
     */
-    public function index($word)
+    public function index($word, $soup)
     {
-        echo "word: $word <br>";
-
-        $soup1 = '3 3
-                    OIE
-                    IIX
-                    EXE';
-        
-        $soup2 = '1 10 EIOIEIOEIO';
-    
-        $soup3 = '5 5
-                    EAEAE
-                    AIIIA
-                    EIOIE
-                    AIIIA
-                    EAEAE';
-    
-        $soup1 = new Soup($soup1);
-        $soup1->setSearch("OIE");
-        $result1 = $soup1->checkSoup();
-        echo "ocurrence: $result1 <br>";
-
-        $soup2 = new Soup($soup2);
-        $soup2->setSearch("OIE");
-        $result2 = $soup2->checkSoup();
-        echo "ocurrence: $result2 <br>";
-    
-        $soup3 = new Soup($soup3);
-        $soup3->setSearch("OIE");
-        $result3 = $soup3->checkSoup();
-        echo "ocurrence: $result3 <br>";
-        // return view('/')->with(['users' => $users, 'pointer'=>'users_list']);
+        $soup = new Soup($soup);
+        $soup->setSearch($word);
+        return $soup->checkSoup();
     }
 }
